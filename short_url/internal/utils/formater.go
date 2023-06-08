db628054f7256741fe3cd7c590f7385913197b25
G33f <G33e33f@gmail.com>
2023-06-08 19:15:52 +0300

    change .gitignore

--- short_url/internal/utils/formater.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "strings"
-
-// FormatQuery removes all tabs and line breaks that can be used in
-// the query for more readable code
-func FormatQuery(q string) string {
-	return strings.ReplaceAll(strings.ReplaceAll(q, "\t", ""), "\n", " ")
-}
